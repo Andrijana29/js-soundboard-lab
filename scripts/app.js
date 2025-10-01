8043843608a9987e7f4ee39982ac08c0e83a65b4
General Assembly 1 <ga-1@Generals-MacBook-Air.local>
2025-10-01 09:36:46 +0200

    finished with a bug

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+//Elements
+const button = document.querySelectorAll(".sound-button")
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const clickedButtonId = e.target.id
+        const audio = new Audio('../sounds/${clickedButtonId}.wav')
+        audio.play()
+    })
+})
